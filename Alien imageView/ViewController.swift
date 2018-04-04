599e14f51a5eff67e102250a4a6c98bb40d73603
D7703_30 <d7703_30@d7703-30ui-imac.local>
2018-04-04 12:38:07 +0900

    imageview02

--- Alien imageView/ViewController.swift
+++ Alien imageView/ViewController.swift
@@ -14,14 +14,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var counter: UILabel!
     var index = 1
+    var direction = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //첫번째 이미지 보이기
         myimageview.image = UIImage(named: "frame1.png")
-        
-        counter.text = String(1)
+        counter.text = "1"
     }
 
     @IBAction func update(_ sender: Any) {
@@ -29,9 +29,16 @@ class ViewController: UIViewController {
        // index = index + 1
         //counter.text = String(index)
         if index == 5 {
-            index = 1
-        } else {
-            index = index + 1
+            direction = 0
+        }
+        else if index == 1 {
+            direction = 1
+        }
+        if direction == 1{
+            index += 1
+        }
+        else if direction == 0{
+            index = index - 1
         }
         
         myimageview.image = UIImage(named: "frame\(index).png")
